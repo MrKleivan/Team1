6fea5b5d165035e583935d1b051f6e22ef2b43db
Tommy Mai <tommymaimai@gmail.com>
2024-10-15 14:18:25 +0200

    added stylesheet folder and file did some minor styling

--- loginController.js
+++ loginController.js
@@ -2,15 +2,15 @@ function authenticateUser(){
     let emailOrUserName = document.getElementById("emailOrUserNameInput").value;
     let password = document.getElementById("passwordInput").value;
 
-    let findUser = model.users.find(u => u.userEmail == emailOrUserName || u.userName == emailOrUserName && u.password == password)
+    let findUser = model.users.find(u => u.userEmail == emailOrUserName || u.userName == emailOrUserName && u.password == password);
 
-    console.log(findUser)
     if(findUser) {
         model.app.loggedInUser = findUser.userId;
-        console.log(model.app.loggedInUser)
         navigateToPage('home')
     }
 }
+
+
 
 function navigateToPage(page){
     model.app.currentPage = page;
--- loginView.js
+++ loginView.js
@@ -1,11 +1,19 @@
 function updateViewLogin() {
     document.getElementById('app').innerHTML = /*HTML*/`
+    <div class="loginForm">
+        <div id="loginElementContainer">
         <h1>Logg inn</h1>
-        Email/Brukernavn<input id="emailOrUserNameInput" value="Skriv Email" oninput="model.inputs.login.email = this.value">
+        <label>Email/Brukernavn</label>
         <br>
-        Passord<input id="passwordInput" oninput="model.inputs.login.password = this.value">
+        <input class="loginInput"  id="emailOrUserNameInput" placeholder="example@gmail.com" oninput="model.inputs.login.email = this.value">
         <br>
-        <button onclick="authenticateUser()">Logg inn</button>
+        <label>Passord</label>
+        <br>
+        <input class="loginInput" id="passwordInput" oninput="model.inputs.login.password = this.value" onkeydown="if(event.code === 'Enter') authenticateUser()" >
+        <br>
+        <button onclick="authenticateUser()" style="margin-left:-5px">Logg inn</button>
         <button onclick="navigateToPage('register')">Registrer</button>
+        </div>
+    </div>
         `;
 }
