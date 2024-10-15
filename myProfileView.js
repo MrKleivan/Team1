a4f7057b1c48f78a27cf799940de3be7595595c1
asyayakim <asyayakimova@yahoo.com>
2024-10-15 15:20:13 +0200

    Added partly save logic and made some css document
    Added possibilities to save data, except interest and photos, created basic layout for page in the separate css document, add photo is not working

--- myProfileView.js
+++ myProfileView.js
@@ -1,42 +1,88 @@
 function updateViewMyProfile() {
     document.getElementById('app').innerHTML = /*HTML*/`
     <h1>Din katteprofil</h1>
-    <div id ="myProfileContent" style= ' flex-direction:column;  flex 1; flex-direction: row; justify-content: center; align-content: center; margin: auto; border-radius:40px; background-color: rgba(255, 255, 255, 0.7);'>
-    <div id="column1" style="flex: 1; display: flex; flex-direction: column; padding-right: 20px;">
-    First Name<br><input id="firstNameInput" type="firstName" placeholder="write your first name" oninput="model.inputs.myProfile.firstName = this.value"><br>
-    Last Name<br><input id="lastNameInput" type="lastName" placeholder="write your last name" oninput="model.inputs.myProfile.firstName = this.value"><br>
-    Favorite Food<br><input id="favouriteFoodInput" type="favouriteFood" placeholder="your cat's favorite food" oninput="model.inputs.myProfile.firstName = this.value"><br>
-    Cat Name<br><input id="catNameInput" type="catName" placeholder="write your cat name" oninput="model.inputs.myProfile.firstName = this.value"><br>
+    <div id="myProfileContent">
+        <div id="column1">
+            First Name<br><input id="firstNameInput" type="text" placeholder="write your first name" oninput="model.inputs.myProfile.firstName = this.value"><br>
+            Last Name<br><input id="lastNameInput" type="text" placeholder="write your last name" oninput="model.inputs.myProfile.lastName = this.value"><br>
+            Favorite Food<br><input id="favouriteFoodInput" type="text" placeholder="your cat's favorite food" oninput="model.inputs.myProfile.favouriteFood = this.value"><br>
+            Cat Name<br><input id="catNameInput" type="text" placeholder="write your cat name" oninput="model.inputs.myProfile.catName = this.value"><br>
+        </div>
+        <div id="column2">
+            Personality<br><input id="personalityInput" type="text" placeholder="personality of your cat" oninput="model.inputs.myProfile.personality = this.value"><br>
+            Color<br><input id="colorInput" type="text" placeholder="color of your cat" oninput="model.inputs.myProfile.color = this.value"><br>
+            Age<br><input id="ageInput" type="number" placeholder="age of your cat" oninput="model.inputs.myProfile.age = this.value"><br>
+            Race<br><input id="raceInput" type="text" placeholder="race of your cat" oninput="model.inputs.myProfile.race = this.value"><br>
+        </div>
     </div>
-    <div id="column2" style="flex: 1; display: flex; flex-direction: column; padding-left: 20px;">
-    Personality<br><input id="personalityInput" type="personality" placeholder="personality of your cat" oninput="model.inputs.myProfile.firstName = this.value"><br>
-    Color<br><input id="colorInput" type="colorDescription" placeholder="color of your cat" oninput="model.inputs.myProfile.firstName = this.value"><br>
-    Age<br><input id="ageInput" type="age" placeholder="age of your cat" oninput="model.inputs.myProfile.firstName = this.value"><br>
-    Race<br><input id="raceInput" type="race" placeholder="race of your cat" oninput="model.inputs.myProfile.firstName = this.value"><br>
+    <div id="noneColumnContent" style="display: flex; flex-direction: column; justify-content: center; align-items: stretch;">
+        Interests<br><select id='interestsSelection' multiple>
+            <option value="cuddle">cuddle</option>
+            <option value="sleep">sleep</option>
+            <option value="play">play</option>
+            <option value="eat">eat</option>
+            <option value="walk">walk</option>
+        </select><br>
     </div>
-    <div id="noneColumnContent">
-    Interests<br><select id='interestsSelection' multiple>
-    <option value="cuddle">cuddle</option>
-    <option value="sleep">sleep</option>
-    <option value="play">play</option>
-    <option value="eat">eat</option>
-    </select><br>
-</div>
+    Add cat pictures
+    <input type='file' id='imageInput' multiple='multiple' accept='image/jpeg, image/png, image/jpg' >
+    <div id='divAddPictures'>
+        <div class= 'dragImageHere'>Add or drag image here</div>
+        <div class= 'dragImageHere'>Add or drag image here</div>
+        <div class= 'dragImageHere'>Add or drag image here</div>
+        <div class= 'dragImageHere'>Add or drag image here</div>
+        <div class= 'dragImageHere'>Add or drag image here</div>
+    </div>
+    
+    Description<br><input id="descriptionInput" type="text" placeholder="describe your cat" oninput="model.inputs.myProfile.description = this.value"><br>
+    <button onclick="saveChanges()">Save</button><br>
 
-    Description<br><input id="descriptionInput" type="description" placeholder="describe your cat" oninput="model.inputs.myProfile.firstName = this.value"><br>
-    <button onclick="saveChanges()">Save</button>
-</div>
-        <br>
-        <button onclick="updateViewHome()">Home</button>
-        <button onclick="updateViewMatch()">match</button>
-        <button onclick="updateViewChat()">chat</button>
+    <button onclick="updateViewHome()">Home</button>
+    <button onclick="updateViewMatch()">Match</button>
+    <button onclick="updateViewChat()">Chat</button>
     `;
+    loadSavedData();
 }
 function saveChanges() {
-    var selectedValues = [];
-    for (var option of document.getElementById('interestsSelection').options) {
-        if (option.selected) {
-            selectedValues.push(option.value)
-        }
-    }
+    const myProfileIdOwner = model.app.loggedInUser ? model.app.loggedInUser.userId : 1;
+    const formData = {
+        firstName: model.inputs.myProfile.firstName,
+        lastName: model.inputs.myProfile.lastName,
+        favouriteFood: model.inputs.myProfile.favouriteFood,
+        catName: model.inputs.myProfile.catName,
+        personality: model.inputs.myProfile.personality,
+        color: model.inputs.myProfile.color,
+        age: model.inputs.myProfile.age,
+        race: model.inputs.myProfile.race,
+        interests: model.inputs.myProfile.interests,
+        description: model.inputs.myProfile.description,
+    };
+    localStorage.setItem('myProfile', JSON.stringify(formData));
+    alert('Profile data saved successfully!');
 }
+function loadSavedData() {
+    const savedData = localStorage.getItem('myProfile');
+    if (savedData) {
+      const formData = JSON.parse(savedData);
+      model.inputs.myProfile.firstName = formData.firstName;
+      model.inputs.myProfile.lastName = formData.lastName;
+      model.inputs.myProfile.favouriteFood = formData.favouriteFood;
+      model.inputs.myProfile.catName = formData.catName;
+      model.inputs.myProfile.personality = formData.personality;
+      model.inputs.myProfile.color = formData.color;
+      model.inputs.myProfile.age = formData.age;
+      model.inputs.myProfile.race = formData.race;
+      model.inputs.myProfile.description = formData.description;
+
+      document.getElementById('firstNameInput').value = formData.firstName;
+      document.getElementById('lastNameInput').value = formData.lastName;
+      document.getElementById('favouriteFoodInput').value = formData.favouriteFood;
+      document.getElementById('catNameInput').value = formData.catName;
+      document.getElementById('personalityInput').value = formData.personality;
+      document.getElementById('colorInput').value = formData.color;
+      document.getElementById('ageInput').value = formData.age;
+      document.getElementById('raceInput').value = formData.race;
+      document.getElementById('descriptionInput').value = formData.description;
+    }
+  }
+
