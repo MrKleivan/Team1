4f4213d83caeaf6c5504bb43c04d8264a8810936
Tommy Mai <tommymaimai@gmail.com>
2024-10-15 11:07:52 +0200

    fixed credential bug

--- loginController.js
+++ loginController.js
@@ -2,8 +2,9 @@ function authenticateUser(){
     let emailOrUserName = document.getElementById("emailOrUserNameInput").value;
     let password = document.getElementById("passwordInput").value;
 
-    let findUser = model.users.find(u => (u.userEmail == emailOrUserName || u.userName) && u.password == password)
+    let findUser = model.users.find(u => u.userEmail == emailOrUserName || u.userName == emailOrUserName && u.password == password)
 
+    console.log(findUser)
     if(findUser) {
         model.app.loggedInUser = findUser.userId;
         console.log(model.app.loggedInUser)
