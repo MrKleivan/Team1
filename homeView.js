4e9c8cce0a0a15704adf602a79b75f444fa6ca41
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2024-10-15 13:34:16 +0200

    up

--- homeView.js
+++ homeView.js
@@ -1,16 +1,23 @@
 function updateViewHome() {
     let myProfile = model.app.loggedInUser;
     let otherProfile = model.users.filter(user => user.userId != myProfile);
+    let otherCatProfile =  model.cats.filter(user => user.userId != myProfile);
+    let catPicture = model.pictures.filter(user => user.id == myProfile);
     let count = Math.floor(Math.random * model.users.length - 1);
     
     
     document.getElementById('app').innerHTML = /*HTML*/`
         <h1 style="font-size:5vh;text-align:center;color:purple;">Finn din kats elsker</h1>
         
-        
-        <div id="homeContent" style="width: 60vh; height: 60vh; align-content: center; margin: auto; border-radius: 40px; background-color: rgba(255, 255, 255, 0.5);">
-            <div class="profileImage" style="width: 55vh; height: 55vh; margin: auto; border-radius: 40px; background-image: url(img/cat2.jpg); background-size: cover;">
-            
+        <div id="homeContent"  style="width: 80vh; height: 60vh; margin: auto; display: flex; align-content:center; text-align: center;">
+            <div style="width: 10%; height: 80%; align-content:center;" >
+                <button style="width: 20px; height: 20px;"></button>
+            </div>
+            <div id="homeProfile" style="width: 80%; height: 80%; align-content: center; border-radius: 40px; background-color: rgba(255, 255, 255, 0.5);">
+                
+            </div>
+            <div style="width: 10%; height: 80%;align-content:center;">
+                <button style="width: 20px; height: 20px;"></button>
             </div>
         </div>
         
