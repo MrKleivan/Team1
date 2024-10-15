a19f07211ec3ad51347e21a74e4ac0107df4d24c
EmTrett <150582441+EmTrett@users.noreply.github.com>
2024-10-15 15:05:30 +0200

    Navigasjons endring i index.html arbeid på sortering i messagesController
    Endret navigasjons knappen i header fra chat side til messages
    
    startet på sortering av meldinger basert på loggedInUser og chatLog:
    
    Dato vises og sorteres riktig
    
    Navn tilknyttet til chat log viser riktig
    
    TODO:
    fjerne duplikat visninger av chat log
    bedre navngivning
    se på mulig hjelpefunksjoner

--- chatView.js
+++ chatView.js
@@ -1,11 +1,6 @@
-let selectedUser = model.users.filter(user => {
-    return user.userId === 1
-});
-selectedUser = selectedUser.pop()
-let loggedInUser = 3;
-
-function updateViewChat() { 
-
+function updateViewChat() {
+    let selectedUser = model.users.filter(user => user.userId === 2);
+    selectedUser = selectedUser.pop() 
     document.getElementById('app').innerHTML = /*HTML*/`
         <div>
 
@@ -13,25 +8,26 @@ function updateViewChat() {
                 ${selectedUser.userName}
             </div><br />
             <div>
-                ${drawChatHtml()}
+                ${drawChatHtml(selectedUser)}
             </div>
         </div>
     `;
 }
 
-function drawChatHtml(){
+function drawChatHtml(selectedUser){
+    let currentUser = model.app.loggedInUser;
     let dateFormat = {weekday: 'short', day: '2-digit', hour: 'numeric', minute: 'numeric'};
 
     let chatLogs = [];
     let html = '';
     console.log(chatLogs)
     model.chatLog.filter(log => {
-        if((log.senderId === loggedInUser && log.recipientId === selectedUser.userId) || (log.senderId === selectedUser.userId && log.recipientId === loggedInUser)){chatLogs.push(log)}
+        if((log.senderId === currentUser && log.recipientId === selectedUser.userId) || (log.senderId === selectedUser.userId && log.recipientId === currentUser)){chatLogs.push(log)}
     })
 
     for(log of chatLogs){
         let isSender = false;
-        if(log.senderId == loggedInUser){isSender = true}
+        if(log.senderId == currentUser){isSender = true}
         let alignMessage = isSender ? 'right' : 'left';
 
         html += `
