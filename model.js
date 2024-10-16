d99ec29505a72ad5c4d55e220121abf30b015801
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2024-10-16 10:57:12 +0200

    Update model.js

--- model.js
+++ model.js
@@ -125,9 +125,6 @@ let model = {
         //TODO: se på mulig bedre navn for placeInSequence
         {userId: 1, pictureUrl: 'img/cat1.jfif', placeInSequence: 1}
         , {userId: 1, pictureUrl: 'img/cat2.jpg', placeInSequence: 2}
-        , {userId: 1, pictureUrl: '', placeInSequence: 3}
-        , {userId: 1, pictureUrl: '', placeInSequence: 4}
-        , {userId: 1, pictureUrl: '', placeInSequence: 5}
     ],
 
     //TODO: Tenk på navn likesUserId    
