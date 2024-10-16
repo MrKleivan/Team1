fb4c581b04d167159572fc445a253ad213ec4d37
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2024-10-16 10:56:55 +0200

    update

--- homeView.js
+++ homeView.js
@@ -1,9 +1,9 @@
+let myProfile = model.app.loggedInUser;
+let otherProfile = model.users.filter(user => user.userId != myProfile);
+let otherCatProfile =  model.cats.filter(user => user.userId != myProfile);
+let catPicture = model.pictures.filter(user => user.id == myProfile);
+let count = 1;
 function updateViewHome() {
-    let myProfile = model.app.loggedInUser;
-    let otherProfile = model.users.filter(user => user.userId != myProfile);
-    let otherCatProfile =  model.cats.filter(user => user.userId != myProfile);
-    let catPicture = model.pictures.filter(user => user.id == myProfile);
-    let count = Math.floor(Math.random * model.users.length - 1);
     
     
     document.getElementById('app').innerHTML = /*HTML*/`
@@ -11,16 +11,16 @@ function updateViewHome() {
         
         <div id="homeContent"  style="width: 100%; height: 80%; margin: auto;align-items: center; display: flex; align-content:center; text-align: center;">
             <div style="width: 10%; height: 95%; align-content:center;" >
-                <button style="width: 20px; height: 20px;"></button>
+            <img style="height: 5vh" src="img/leftArrow.svg" alt="Home Icon"  onclick="count > 0 ? count-- : count = 0" />
             </div>
             <div id="homeProfile" style="width: 95%; height: 90%; border-radius: 40px; align-content: center; background-color: rgba(255, 255, 255, 0.5);">
-                <div class="profileView" style=" width: 90%; height: 90%; margin: auto; border-radius: 40px; background-image: url(${catPicture[0].pictureUrl});background-size: cover;">
+                <div class="profileView" style=" width: 90%; height: 90%; margin: auto; border-radius: 40px; background-image: url(${catPicture[count].pictureUrl});background-size: cover;background-position: center; background-repeat: no-repeat;">
                     <h1>${otherCatProfile[0].name}<h1>
                     <h1 class="profileScript">${otherCatProfile[0].description}<h1>
                 </div>
             </div>
             <div style="width: 10%; height: 10%; align-content:center;">
-                <button style="width: 20px; height: 20px;"></button>
+            <img style="height: 5vh" src="img/rightArrow.svg" alt="Home Icon"  onclick="count < catPicture.lenght ? count++ : count = Number(catPicture.length)" />
             </div>
         </div>
         
