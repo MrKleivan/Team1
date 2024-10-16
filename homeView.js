2f928c3687e9429b3c75eb5b9fe0f499e53b3ac1
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2024-10-16 10:25:13 +0200

    update

--- homeView.js
+++ homeView.js
@@ -7,28 +7,28 @@ function updateViewHome() {
     
     
     document.getElementById('app').innerHTML = /*HTML*/`
-        <h1 style="font-size:5vh;text-align:center;color:purple;">Finn din kats elsker</h1>
+        <h1 style="font-size:3vh;text-align:center;color:purple;">Finn din kats elsker</h1>
         
-        <div id="homeContent"  style="width: 80vh; height: 60vh; margin: auto; display: flex; align-content:center; text-align: center;">
-            <div style="width: 10%; height: 80%; align-content:center;" >
+        <div id="homeContent"  style="width: 100%; height: 80%; margin: auto;align-items: center; display: flex; align-content:center; text-align: center;">
+            <div style="width: 10%; height: 95%; align-content:center;" >
                 <button style="width: 20px; height: 20px;"></button>
             </div>
-            <div id="homeProfile" style="width: 80%; height: 80%; border-radius: 40px; align-content: center; background-color: rgba(255, 255, 255, 0.5);">
+            <div id="homeProfile" style="width: 95%; height: 90%; border-radius: 40px; align-content: center; background-color: rgba(255, 255, 255, 0.5);">
                 <div class="profileView" style=" width: 90%; height: 90%; margin: auto; border-radius: 40px; background-image: url(${catPicture[0].pictureUrl});background-size: cover;">
                     <h1>${otherCatProfile[0].name}<h1>
                     <h1 class="profileScript">${otherCatProfile[0].description}<h1>
                 </div>
             </div>
-            <div style="width: 10%; height: 80%;align-content:center;">
+            <div style="width: 10%; height: 10%; align-content:center;">
                 <button style="width: 20px; height: 20px;"></button>
             </div>
         </div>
         
         
         <br/>
-        <div style="width: 20vw; margin: auto; text-align: center;">
-            <button onclick="likeCat()" style="width: 5vw; height: 4vh; border: none; margin-right: 20px; border-radius: 10px; background-color: rgba(255,255,255,0.7);">Liker</button>
-            <button onclick="notLikeCat()" style="width: 5vw; height: 4vh; border: none; border-radius: 10px; background-color: rgba(255,255,255,0.7);">Liker ikke</button>
+        <div style="width: 90%; margin: auto; text-align: center;">
+            <button onclick="likeCat()" style="width: 10vw; height: 4vh; border: none; margin-right: 40px; border-radius: 10px; background-color: rgba(255,255,255,0.7);">Liker</button>
+            <button onclick="notLikeCat()" style="width: 10vw; height: 4vh; border: none; border-radius: 10px; background-color: rgba(255,255,255,0.7);">Liker ikke</button>
         </div>
         `;
     
