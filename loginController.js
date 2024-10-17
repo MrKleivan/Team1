17f9bc1bd2c8042345342c7dede0ebf2b9625909
Tommy Mai <tommymaimai@gmail.com>
2024-10-17 14:28:45 +0200

    changed unittests so it reflects the code in controllers

--- loginController.js
+++ loginController.js
@@ -2,12 +2,16 @@ function authenticateUser(){
     let emailOrUserName = document.getElementById("emailOrUserNameInput").value;
     let password = document.getElementById("passwordInput").value;
 
-    let findUser = model.users.find(u => u.userEmail == emailOrUserName || u.userName == emailOrUserName && u.password == password);
+    authenticateLoginData(emailOrUserName, password)
 
     if(findUser) {
         model.app.loggedInUser = findUser.userId;
         navigateToPage('home')
     }
+}
+
+function authenticateLoginData(inputEmailOrUserName, inputPassword){
+    return findUser = model.users.find(u => u.userEmail == inputEmailOrUserName || u.userName == inputEmailOrUserName && u.password == inputPassword);
 }
 
 function navigateToPage(page){
